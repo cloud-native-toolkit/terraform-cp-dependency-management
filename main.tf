ec2cd3c80932f9f20ed1e016a208a953ce04af1f
Gowdhaman J <gowdhaman_j@yahoo.com>
2022-02-04 21:32:47 +0530

    Updated the Channel subscription and license version (#12)
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
    
    Co-authored-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- main.tf
+++ main.tf
@@ -1,31 +1,32 @@
+## As of Feb/4/2022 this module resolve the dependency for CP4i version 2021.4.1
 locals {
   platform_navigator = {
-    channel = "v4.1-eus"
-    version = "2020.4.1-eus"
-    license = "L-RJON-BUVMQX"
+    channel = "v5.2"
+    version = "2021.4.1"
+    license = "L-RJON-C7QG3S"
   }
   mq = {
-    channel = "v1.3-eus"
-    version = "9.2.0.1-r1-eus"
-    license = "L-RJON-BUVMQX"
+    channel = "v1.7"
+    version = "9.2.4.0-r1"
+    license = "L-RJON-C7QG3S"
     license_use = "NonProduction"
   }
   ace = {
-    channel = "v1.1-eus"
-    version = ""
-    license = ""
-    license_use = ""
+    channel = "v3.0"
+    version = "12.0.2.0-r2"
+    license = "L-KSBM-C87FU2"
+    license_use = "CloudPakForIntegrationNonProduction"
   }
   apic = {
-    channel = "v2.1-eus"
-    version = "10.0.3.0-ifix1"
-    license = ""
-    license_use = ""
+    channel = "v2.4"
+    version = "10.0.4.0-ifix1-54"
+    license = "L-RJON-C7BJ42"
+    license_use = "nonproduction"
   }
   eventstreams = {
-    channel = "v2.2-eus"
-    version = "10.2.0-eus"
+    channel = "v2.5"
+    version = "10.5.0"
     license = ""
-    license_use = ""
+    license_use = "CloudPakForIntegrationNonProduction"
   }
 }
