d4d72d7efeeaf614fbae18c69a0e14dbb8225bc8
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-03-06 12:07:13 +0530

    Updated the module to support future CP4i version
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -1,135 +1,81 @@
 
-variable "gitops_config" {
-  type        = object({
-    boostrap = object({
-      argocd-config = object({
-        project = string
-        repo = string
-        url = string
-        path = string
-      })
-    })
-    infrastructure = object({
-      argocd-config = object({
-        project = string
-        repo = string
-        url = string
-        path = string
-      })
-      payload = object({
-        repo = string
-        url = string
-        path = string
-      })
-    })
-    services = object({
-      argocd-config = object({
-        project = string
-        repo = string
-        url = string
-        path = string
-      })
-      payload = object({
-        repo = string
-        url = string
-        path = string
-      })
-    })
-    applications = object({
-      argocd-config = object({
-        project = string
-        repo = string
-        url = string
-        path = string
-      })
-      payload = object({
-        repo = string
-        url = string
-        path = string
-      })
-    })
-  })
-  description = "Config information regarding the gitops repo structure"
+variable cluster_username { 
+  type        = string
+  description = "The username for AWS access"
 }
 
-variable "git_credentials" {
-  type = list(object({
-    repo = string
-    url = string
-    username = string
-    token = string
-  }))
-  description = "The credentials for the gitops repo(s)"
-  sensitive   = true
+variable "cluster_password" {
+  type        = string
+  description = "The password for AWS access"
+}
+
+variable "server_url" {
+  type        = string
+}
+
+variable "bootstrap_prefix" {
+  type = string
+  default = ""
 }
 
 variable "namespace" {
   type        = string
-  description = "The namespace where the application should be deployed"
+  description = "Namespace for tools"
 }
 
-variable "kubeseal_cert" {
+variable "cluster_name" {
   type        = string
-  description = "The certificate/public key used to encrypt the sealed secrets"
+  description = "The name of the cluster"
   default     = ""
 }
 
-variable "server_name" {
+variable "cluster_type" {
   type        = string
-  description = "The name of the server"
-  default     = "default"
+  description = "The type of cluster that should be created (openshift or kubernetes)"
+  default     = "ocp4"
 }
 
-variable "entitlement_key" {
+variable "cluster_exists" {
   type        = string
-  description = "The entitlement key required to access Cloud Pak images"
-  sensitive   = true
+  description = "Flag indicating if the cluster already exists (true or false)"
+  default     = "true"
 }
 
-#Module Specific extension
-variable "is_ace_dash_required_dedicated_ns" {
-  type = bool
-  description = "If ACE Dashboard instance need to be deployed in dedicated namespace"
-  default = false
-
-  
-}
-variable "ace_dash_instance_namespace" {
-  type = string
-  description = "It is beeter to manage ACE Dashboard instance workload in a dedicated namespace"
-  default = "gitops-cp-ace-dashboard"
-}
-
-variable "ace_version" {
+variable "git_token" {
   type        = string
-  description = "The version of the ACE Dashboard should be installed"
-  default     = ""
+  description = "Git token"
 }
 
-variable "license" {
+variable "git_host" {
   type        = string
-  description = "The license string that should be used for the instance"
-  default     = ""
+  default     = "github.com"
 }
 
-variable "license_use" {
-  type        = string
-  description = "The possible values are CloudPakForIntegrationNonProduction or CloudPakForIntegrationProductionn"
-  default     = ""
+variable "git_type" {
+  default = "github"
+}
+
+variable "git_org" {
+  default = "cloud-native-toolkit-test"
+}
+
+variable "git_repo" {
+  default = "git-module-test"
+}
+
+variable "gitops_namespace" {
+  default = "openshift-gitops"
+}
+
+variable "git_username" {
+}
+
+variable "kubeseal_namespace" {
+  default = "sealed-secrets"
+}
+
+variable "cp_entitlement_key" {
 }
 
 
-#If ACE Dashboad Instance needed to be overridden then use this
-variable "ace_dash_instance_name" {
-  type = string
-  description = "If ACE Dashboard instance name needed to be overridden"
-  default = ""
-  
-}
-#If ACE Dashboad Instance needed to be overridden then use this
-variable "storage_class_name" {
-  type = string
-  description = "RWX Accessmode supported Storageclass is required "
-  default = "portworx-db2-rwo-sc"
-  
-}
+
