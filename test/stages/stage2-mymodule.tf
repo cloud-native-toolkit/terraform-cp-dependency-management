4b62fef8124caabe1d6b27cbd74b5647a06ad515
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-03-06 12:11:51 +0530

    Updated the module to support future CP4i version
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -1,3 +1,6 @@
 module "dependency-management" {
   source = "./module"
+  #The CP4i Version. Possible values are (2021_4_1 , 2022_1_1). 
+  #If no values is set by default this will refer to Latest available CP4i version
+  cp4i_version="2021_4_1"
 }
