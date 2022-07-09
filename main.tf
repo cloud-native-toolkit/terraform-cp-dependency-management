b739f2c00a2b1d60f964adac191ed26b194abcce
gowdhaman-j <gowdhaman_j@yahoo.com>
2022-07-09 09:24:06 +0530

    Update for CP4i-2022.2.1
    Signed-off-by: gowdhaman-j <gowdhaman_j@yahoo.com>

--- main.tf
+++ main.tf
@@ -7,65 +7,66 @@ cp4i_version=var.cp4i_version == ""? "default" : "cp4i_${var.cp4i_version}"
    
     # Default will be the Latest available CP4i Version.
     default={
+
       platform_navigator = {
-      channel = "v5.2"
-      version = "2021.4.1"
-      license = "L-RJON-C7QG3S"
+      channel = "v6.0"
+      version = "2022.1.1"
+      license = "L-RJON-CD3JKX"
     }
     mq = {
-      channel = "v1.7"
-      version = "9.2.4.0-r1"
-      license = "L-RJON-C7QG3S"
+      channel = "v2.0"
+      version = "9.3.0.0-r1"
+      license = "L-RJON-CD3JKX"
       license_use = "NonProduction"
     }
     ace = {
-      channel = "v3.0"
-      version = "12.0.2.0-r2"
-      license = "L-KSBM-C87FU2"
+      channel = "v5.0-lts"
+      version = "12.0.5.0-r1-lts"
+      license = "L-APEH-CCHL5W"
       license_use = "CloudPakForIntegrationNonProduction"
     }
     apic = {
-      channel = "v2.4"
-      version = "10.0.4.0-ifix1-54"
-      license = "L-RJON-C7BJ42"
+      channel = "v3.0"
+      version = "10.0.5.0"
+      license = "L-RJON-CD3JHD"
       license_use = "nonproduction"
     }
     eventstreams = {
-      channel = "v2.5"
-      version = "10.5.0"
+      channel = "v3.0"
+      version = "11.0.2"
       license = ""
       license_use = "CloudPakForIntegrationNonProduction"
     }
-    
+   
     asset_repository = {
-      channel = "v1.4"
-      version = "2021.4.1"
-      license = "L-PNAA-C68928"
+      channel = "v1.5"
+      version = "TBD"
+      license = "TBD"
       license_use = "CloudPakForIntegrationNonProduction"
     }
     
     operations_dashboard = {
-      channel = "v2.5"
-      version = "2021.4.1"
-      license = "CP4I"
+      channel = "v2.6-lts"
+      version = "TBD"
+      license = "TBD"
       license_use = "CloudPakForIntegrationNonProduction"
     }
 
     aspera = {
-      channel = "v1.1"
-      version = "2021.4.1"
-      license = "CP4I"
+      channel = "v1.5"
+      version = "TBD"
+      license = "TBD"
       license_use = "CloudPakForIntegrationNonProduction"
     }
 
     datapower = {
-      channel = "v1.5"
-      version = "10.0-cd"
-      license = "L-RJON-CCCP46"
-      license_use="nonproduction"
+      channel = "v1.6"
+      version = "10.5.0.0"
+      license = "L-RJON-CCAT5F"
+      license_use = "nonproduction"
     }
-
-  }
+        
+    }
 
     # CP4i Version 2021.4.1
     cp4i_2021_4_1={
@@ -129,72 +130,68 @@ cp4i_version=var.cp4i_version == ""? "default" : "cp4i_${var.cp4i_version}"
 
 }
 
-  # CP4i Version 2022.1.1
-  cp4i_2022_1_1={
+  # CP4i Version 2022.2.1
+  cp4i_2022_2_1={
       platform_navigator = {
-      channel = "TBD"
+      channel = "v6.0"
       version = "2022.1.1"
-      license = "TBD"
+      license = "L-RJON-CD3JKX"
     }
     mq = {
-      channel = "TBD"
-      version = "TBD"
-      license = "TBD"
-      license_use = "TBD"
+      channel = "v2.0"
+      version = "9.3.0.0-r1"
+      license = "L-RJON-CD3JKX"
+      license_use = "NonProduction"
     }
     ace = {
-      channel = "TBD"
-      version = "TBD"
-      license = "TBD"
-      license_use = "TBD"
+      channel = "v5.0-lts"
+      version = "12.0.5.0-r1-lts"
+      license = "L-APEH-CCHL5W"
+      license_use = "CloudPakForIntegrationNonProduction"
     }
     apic = {
-      channel = "TBD"
-      version = "TBD"
-      license = "TBD"
-      license_use = "TBD"
+      channel = "v3.0"
+      version = "10.0.5.0"
+      license = "L-RJON-CD3JHD"
+      license_use = "nonproduction"
     }
     eventstreams = {
-      channel = "TBD"
-      version = "TBD"
+      channel = "v3.0"
+      version = "11.0.2"
       license = ""
-      license_use = "TBD"
+      license_use = "CloudPakForIntegrationNonProduction"
     }
    
     asset_repository = {
-      channel = "TBD"
+      channel = "v1.5"
       version = "TBD"
       license = "TBD"
-      license_use = "TBD"
+      license_use = "CloudPakForIntegrationNonProduction"
     }
     
     operations_dashboard = {
-      channel = "TBD"
+      channel = "v2.6-lts"
       version = "TBD"
       license = "TBD"
-      license_use = "TBD"
+      license_use = "CloudPakForIntegrationNonProduction"
     }
 
     aspera = {
-      channel = "TBD"
+      channel = "v1.5"
       version = "TBD"
       license = "TBD"
-      license_use = "TBD"
+      license_use = "CloudPakForIntegrationNonProduction"
     }
 
     datapower = {
-      channel = "TBD"
-      version = "TBD"
-      license = "TBD"
-      license_use = "TBD"
+      channel = "v1.6"
+      version = "10.5.0.0"
+      license = "L-RJON-CCAT5F"
+      license_use = "nonproduction"
     }
   }
 
 }
-
-
-
-
 
 
 }
